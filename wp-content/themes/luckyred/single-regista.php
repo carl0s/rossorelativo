de36bf14e4593a377f0ba424c27b4cad26dc3597
dungbotta <dungbotta@MacBook-Pro-di-Dung.local>
2014-05-12 11:59:52 +0200

    update

--- wp-content/themes/luckyred/single-regista.php
+++ wp-content/themes/luckyred/single-regista.php
@@ -2,19 +2,27 @@
 
 <?php wp_reset_postdata();  ?>
 
-<div class="row film-uno">
-  <?php // query_posts('post_type=movie'); ?>
-  <div class="large-4 columns">
-    <?php echo get_the_post_thumbnail($movie->ID); ?>
-  </div>
-  <div class="large-8 columns">
-    <?php //if (have_posts()) : while(have_posts()) : the_post(); ?>
-    <h1><?php echo get_the_title(); ?></h1>
-    </h6>
-    <p>
-      <?php the_content(); ?>
-    </p>
-    <?php //endwhile; endif; ?>
+<div class="img-regista slideshow-wrapper">
+    <div class="fotorama slideshow" data-allowfullscreen="native" data-width="100%" data-ratio="1440/750">
+      <div class="slide">
+        <div class='info-wrapper'>
+          <?php echo get_the_post_thumbnail(); ?>
+          <div class='row'>
+            <div class='info-title large-6 columns'>
+              <?php the_title('<h2>','</h2>'); ?>
+            </div>
+            <div class='info-date large-1 large-offset-4 columns'>
+              <?php 
+              $date = DateTime::createFromFormat('Ymd', get_field('data_di_uscita'));
+              ?>
+              <h6>dal</h6>
+              <h4><?php echo $date->format('d'); ?></h4>
+              <h5><?php echo $date->format('M'); ?></h5>
+              <h6>al cinema</h6>
+            </div>
+          </div>
+        </div>
+      </div>
   </div>
 </div>
 
