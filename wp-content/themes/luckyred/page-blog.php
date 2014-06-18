8dcb41139d33d2cef7018284ad59a2a9c4b2ede8
Tommaso <tommicos@gmail.com>
2014-06-18 22:09:53 +0200

    Update responsive page-blog.php

--- wp-content/themes/luckyred/page-blog.php
+++ wp-content/themes/luckyred/page-blog.php
@@ -5,6 +5,9 @@
 ?>
 <?php get_header(); ?>
 
+<section class="main-section">
+  <!-- content goes here -->
+
 <?php wp_reset_postdata(); ?>
 
 <?php
@@ -32,10 +35,10 @@ $actual_id = get_the_ID();
     <?php echo get_the_post_thumbnail(); ?>
     <div class='row'>
       <div class="info-blog">
-        <div class='info-date large-2 columns'>
+        <div class='info-date large-2 small-5 columns'>
           <h6><?php echo get_the_date('d F Y'); ?></h6>
         </div>
-        <div class='info-title'>
+        <div class='info-title large-12 small-12 columns'>
           <h2><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h2>
         </div>  
         <div class="row collapse">
@@ -50,6 +53,13 @@ $actual_id = get_the_ID();
 <?php endif; ?>
 <?php endwhile; endif; ?> 
 <?php wp_reset_query(); ?>
+</div>
+
+</section>
+
+  <a class="exit-off-canvas"></a>
+
+  </div>
 </div>
 
 <?php get_footer(); ?>
