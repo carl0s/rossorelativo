5ccafef983ad5c1951ad4d82e09ad017a129e53d
dungbotta <dungbotta@MacBook-Pro-di-Dung.local>
2014-06-18 13:07:24 +0200

    Update

--- wp-content/themes/luckyred/single-regista.php
+++ wp-content/themes/luckyred/single-regista.php
@@ -1,8 +1,8 @@
   <?php get_header(); ?>
 
-<?php wp_reset_postdata();  ?>
+  <?php wp_reset_postdata();  ?>
 
-<div class="img-regista slideshow-wrapper">
+  <div class="img-regista slideshow-wrapper">
     <div class="fotorama slideshow" data-allowfullscreen="native" data-width="100%" data-ratio="1440/750">
       <div class="slide">
         <div class='info-wrapper'>
@@ -29,72 +29,68 @@
           </div>
         </div>
       </div>
+    </div>
   </div>
-</div>
 
-<div class="biografia large-12 columns">
-  <div class="row">
-    <h2>Biografia</h2>
-    <p><?php echo get_the_content(); ?></p>
+  <div class="biografia large-12 columns">
+    <div class="row">
+      <h2>Biografia</h2>
+      <p><?php echo get_the_content(); ?></p>
+    </div>
   </div>
-</div>
 
-<div class="filmografia large-12 columns">
-  <div class="row">
-    <h2>Filmografia</h2>
-    <h3>Film dal catalogo lucky red:</h3>
-        <?php if (have_posts()) : while(have_posts()) : the_post() ; ?>
-        <div>
+  <div class="filmografia large-12 columns">
+    <div class="row">
+      <h2>Filmografia</h2>
+      <h3>Film dal catalogo lucky red:</h3>
+      <?php if (have_posts()) : while(have_posts()) : the_post() ; ?>
+      <div>
 
-          <?php 
+        <?php 
         $filmografie = get_posts(array(
           'post_type'  => 'film',
           'meta_query' => array(
-                array(
+            array(
                   'key' => 'regia', // name of custom field
                   'value' => '"' . get_the_ID() . '"', // matches exaclty "123", not just 123. This prevents a match for "1234"
                   'compare' => 'LIKE'
-                )
-                )
+                  )
+            )
           ));
           ?>
 
           <?php if( $filmografie ): ?>
-              <?php foreach( $filmografie as $filmografia ): ?>
-                <?php 
-                $id = $filmografia->ID;
-                $photo = get_field('locandina', $filmografia->ID);
-                ?>
-                <div class="film-regista large-4 columns">
-                  <div class="title-layout">
-                    <a href="<?php get_the_permalink($id); ?>"><h3 class="titolo-film-regista"> <?php echo $filmografia->post_title; ?></h3></a>
-                  </div>
+          <?php foreach( $filmografie as $filmografia ): ?>
+          <?php 
+          $id = $filmografia->ID;
+          $photo = get_field('locandina', $filmografia->ID);
+          ?>
+          <div class="film-regista large-4 columns">
+            <div class="title-layout">
+              <a href="<?php get_the_permalink($id); ?>"><h3 class="titolo-film-regista"> <?php echo $filmografia->post_title; ?></h3></a>
+            </div>
 
-                <a href="<?php get_permalink(); ?>"><?php echo get_the_post_thumbnail($filmografia->ID); ?>"</a>
-                </div>
-                
-                  
-              <?php endforeach; ?>
-              
-            <?php endif; ?>
+            <a href="<?php get_permalink(); ?>"><?php echo get_the_post_thumbnail($filmografia->ID); ?></a>
+          </div>
 
-        </div>
-        
-      <?php endwhile; endif; ?>
-  </div>
+        <?php endforeach; ?>
+
+      <?php endif; ?>
+
+    </div>
+
+  <?php endwhile; endif; ?>
+</div>
 </div>
 
 <div class="altri-film-bg large-12 columns">
   <div class="row">
     <h3><span>Altri film</span></h3>
     <div class="altri-film-regista">
-<?php query_posts(array('post_type'=>'regista')); ?>
-    <h5><?php echo get_field('film_regista'); ?></h5>
-
+      <?php query_posts(array('post_type'=>'regista')); ?>
+      <h5><?php echo get_field('film_regista'); ?></h5>
     </div>
-    
-   
   </div>
 </div>
-    <!--FOOTER-->
-    <?php get_footer(); ?>
+<!--FOOTER-->
+<?php get_footer(); ?>
